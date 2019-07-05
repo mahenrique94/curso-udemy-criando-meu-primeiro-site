8c054c4503ff99b04e2296a599e855ec2e5a918a
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2019-07-04 22:36:04 -0300

    Finalizando página inicial

---
+++ assets/js/type.js
@@ -0,0 +1,12 @@
+new Typed('.js-panelSubtitle', {
+    backSpeed: 70,
+    loop: true,
+    showCursor: false,
+    smartBackspace: false,
+    strings: [
+        'Desenvolvedor Front-End',
+        'Desenvolvedor Back-End',
+        'UI/UX'
+    ],
+    typeSpeed: 50
+})
